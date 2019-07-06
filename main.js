244ca0cbc47ce795fca5ede2f6f7eb730135c1c3
Zireael07 <zi@mail.com>
2019-07-06 15:00:37 +0200

    new: wrap up part 1 of roguelike tutorial (main loop and key handler)

--- main.js
+++ main.js
@@ -1,17 +1,47 @@
+var player = {
+    _x: 0,
+    _y: 0,
+
+    move: function(x,y) {
+        this._x = this._x + x;
+        this._y = this._y + y;
+    }
+}
+
+
 var Game = {
     canvas: null,
     context: null,
+    player: null,
 
     newGame: function(cnv) {
         this.canvas = cnv;
         this.context = cnv.getContext("2d");
+        this.player = player;
     },
+
+    //rendering functions from here down
     clearGame: function() {
         this.context.fillStyle = 'rgb(0,0,0)';
         this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
     },
+    renderPlayer: function(){
+        this.renderGfxTile(resources.get("gfx/human_m.png"), this.player._x, this.player._y);
+    },
     renderGfxTile: function(img, x, y) {
-        this.context.drawImage(img, x, y);
+        this.context.drawImage(img, x*32, y*32);
+    }
+}
+
+// main key input handler
+// key is the key code
+function processKeyDown(key, player){
+    switch (key) {
+      case 37: player.move(-1, 0); break;  //left
+      case 39: player.move(1, 0);  break;   //right
+      case 38: player.move(0, -1); break;     //up
+      case 40: player.move(0, 1);  break;    //down
+      default: console.log(key);
     }
 }
 
@@ -20,9 +50,18 @@ function setup(canvas) {
     //setup game
     Game.newGame(canvas);
 
-    //test
-    Game.clearGame();
-    Game.renderGfxTile(resources.get("gfx/human_m.png"), 0, 0);
+    //what it says on the tin
+    function mainLoop() {
+        //test
+        Game.clearGame();
+        Game.renderPlayer();
+        requestAnimationFrame(mainLoop);
+    }
+    
+    //crucial! not part of main loop!
+    requestAnimationFrame(mainLoop);
+
+
 }
 
 
@@ -38,7 +77,14 @@ window.onload = function() {
     ]);
     resources.setReady(setup, canvas);
 
-    
+    //keys
+    function onKeyDown(event) {
+        // prevent scrolling on arrow keys
+        event.preventDefault();
+        processKeyDown(event.keyCode, Game.player);
+    }
+
+    window.addEventListener("keydown", onKeyDown)
 
 
 
